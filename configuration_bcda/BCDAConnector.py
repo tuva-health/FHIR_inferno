ecf5a3be412ad92b6d6b7dc483281598be89b4e9
sarah-tuva <sarah@tuvahealth.com>
2023-05-18 16:49:33 -0600

    Add comments to connector script

--- configuration_bcda/BCDAConnector.py
+++ configuration_bcda/BCDAConnector.py
@@ -1,10 +1,12 @@
-# this is just so you can import parseFhir from the parent folder
-# if parseFhir.py is in the same folder as claimsConnector, or if its in a folder in Path, this isnt needed
+# This is just so you can import parseFhir from the parent folder
+# if parseFhir.py is in the same folder as connector this isn't needed.
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Import the parser script
 import parseFhir
 
-parseFhir.parse( r"config\configCoverage.ini")
-parseFhir.parse( r"config\configExplanationOfBenefit.ini")
-parseFhir.parse( r"config\configPatient.ini")
+# Update config paths as needed
+parseFhir.parse(r"config/configCoverage.ini")
+parseFhir.parse(r"config/configPatient.ini")
+parseFhir.parse(r"config/configExplanationOfBenefit.ini")
