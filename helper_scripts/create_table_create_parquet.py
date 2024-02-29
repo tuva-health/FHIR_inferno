17f5ab78657f797a79ffb7a741de72344311080d
tuvaforrest <forrest@tuvahealth.com>
2024-02-29 07:05:57 -0500

    major refactor to parseFhir.py added configs for Health Gorilla Added helper functions updated readmes

---
+++ helper_scripts/create_table_create_parquet.py
@@ -0,0 +1,103 @@
+import os
+import configparser
+
+### configuration
+config_folder = r'config_files'         ## folder with config files to analyze
+filename = 'create_table_scripts.sql'   ## output filename
+schema = 'RAW_DATA',                    ## schema to build
+convert_date_types = True,              ## whether or not to analyze column_names to identifiy dates and datetimes
+stage = '@my_stage/FHIR_Output/'        ## name of the stage in which transformed fhir is located
+
+
+
+
+
+
+### Script
+# Function to format the table name based on the file name
+def format_table_name(file_name):
+    if file_name.startswith("config_"):
+        file_name = file_name[7:]
+    return file_name.replace('.ini', '').replace('.', '_')
+
+# Function to determine the data type of a column
+def get_data_type(column_name, date_types):
+    if date_types:
+        if column_name.endswith("datetime") or column_name.endswith("time") or column_name.lower() == "processed_date":
+            return "timestamp"
+        elif column_name.endswith("date"):
+            return "date"
+    return "varchar"
+
+# Function to create a COPY INTO statement
+def create_copy_statement(table_name, stage, schema):
+    split_parts = table_name.split('_', 1)
+    if len(split_parts) == 1:
+        split_parts.append("[^_]+")
+    else:
+        split_parts[1] = ".*_" + split_parts[1] + ".*"
+    # pattern = f"'{split}_[^.]+\\.csv'"  # Regex pattern for matching file names
+    pattern = f"'.*{split_parts[0]}_{split_parts[1]}'"
+
+
+    return f"""
+-- COPY INTO statement for {table_name}
+COPY INTO {schema + '.' if schema else ''}{table_name}
+FROM {stage}{table_name}/
+FILE_FORMAT = (type = PARQUET)
+MATCH_BY_COLUMN_NAME =  CASE_INSENSITIVE
+"""
+
+# Function to create a SELECT statement
+def create_select_statement(table_name, schema):
+    return f"""
+-- SELECT statement for {table_name}
+SELECT * FROM {schema + '.' if schema else ''}{table_name};
+"""
+
+def process_ini_files(input_dir, output_file, schema=None, date_types=False, stage=None):
+    all_copies = ''
+    all_selects = ''
+
+    # Open the output file
+    with open(output_file, 'w') as output:
+        # Loop through each file in the input directory
+        for file in os.listdir(input_dir):
+            if file.endswith(".ini"):
+                # Reinitialize configparser for each file
+                config = configparser.ConfigParser()
+                config.read(os.path.join(input_dir, file))
+
+                # Check if 'Struct' section exists
+                if 'Struct' in config:
+                    table_name = format_table_name(file)
+                    columns = config['Struct']
+
+                    # Construct the CREATE TABLE statement
+                    create_statement = f"CREATE OR REPLACE TABLE {'.'.join([schema, table_name]) if schema else table_name} (\n"
+                    create_statement += ",\n".join([f"  {col} {get_data_type(col, date_types)}" for col in columns])
+                    create_statement += "\n);\n"
+
+                    # Write to output file
+                    output.write(create_statement)
+
+                    # Write COPY INTO statement if stage is provided
+                    output.write("\n/*\n")
+                    if stage:
+                        copy_statement = create_copy_statement(table_name, stage, schema)
+                        all_copies += copy_statement + "\n"
+                        output.write( copy_statement )
+
+                    # Write SELECT statement
+                    select_statement = create_select_statement(table_name, schema)
+                    all_selects += select_statement + "\n"
+                    output.write( select_statement + "\n*/\n\n")
+
+        # Write all COPY INTO and SELECT statements at the end of the file
+        output.write("/*\n")
+        if stage:
+            output.write("-- COPY STATEMENTS\n" + all_copies + "\n")
+        output.write("-- SELECT STATEMENTS\n" + all_selects)
+        output.write("*/")
+
+process_ini_files(config_folder, filename, schema, convert_date_types, stage)
--- parseFhir.py
+++ parseFhir.py
@@ -3,6 +3,8 @@ import sys
 import csv
 import configparser
 import logging
+import os
+from datetime import datetime
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,7 +12,77 @@ logging.basicConfig(
 )
 
 
-##### functions #####
+
+def extract_paths(json_obj, current_path='', all_paths=None, ignore_paths=None):
+    if all_paths is None:
+        all_paths = set()
+    if ignore_paths is None:
+        ignore_paths = set()
+
+    if isinstance(json_obj, dict):
+        for k, v in json_obj.items():
+            new_path = f"{current_path}.{k}" if current_path else k
+            if new_path not in ignore_paths:
+                extract_paths(v, new_path, all_paths, ignore_paths)
+    elif isinstance(json_obj, list):
+        for idx, item in enumerate(json_obj):
+            extract_paths(item, f"{current_path}.{idx}", all_paths, ignore_paths)
+    else:
+        if current_path != '':
+            all_paths.add(current_path)
+    return all_paths
+
+
+
+def compare_and_write_new_paths(json_obj, filename, anchor, config_paths, output_file):
+    new_paths = set()
+    all_paths = set()
+
+
+    if config_paths.has_section('ignore_paths'):
+        ignore_path_set = set(config_paths['ignore_paths'].values())
+    else:
+        ignore_path_set = None
+
+    if anchor:
+        anchor_obj = get_sub_object(json_obj, anchor)
+
+        if isinstance(anchor_obj, list):
+            for item in anchor_obj:
+                item_paths = extract_paths(item, ignore_paths=ignore_path_set)
+                all_paths.update(item_paths)
+        else:
+            all_paths = extract_paths(anchor_obj, ignore_paths=ignore_path_set)
+        new_paths = all_paths - {path[len("Anchor:"):] if path.startswith("Anchor:") else path for path in config_paths['anchor_paths'].values()}
+    else:
+        all_paths = extract_paths(json_obj, ignore_paths=ignore_path_set)
+        new_paths = all_paths - set(config_paths['root_paths'].values())
+
+
+
+    if new_paths:
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with open(output_file, 'a') as file:
+
+            for path in new_paths:
+                if anchor:
+                    file.write(f'"{filename.name}","Anchor:{path}","{anchor}"\n')
+                else:
+                    file.write(f'"{filename.name}","{path}",""\n')
+
+
+def get_sub_object(obj, path):
+    for part in path.split('.'):
+        if part.isdigit():
+            part = int(part)  # Convert to integer for list indices
+        try:
+            obj = obj[part]
+        except (TypeError, KeyError, IndexError):
+            # Handle the case where the path does not exist in the object
+            return None
+    return obj
+
+
 def is_integer(n):
     try:
         float(n)
@@ -20,7 +92,7 @@ def is_integer(n):
         return float(n).is_integer()
 
 
-def getJsonValue(lnjsn, ln):
+def getJsonValue(lnjsn, ln,filename = ""):
     retVal = ""
     for x in ln.split("."):
         if x.startswith("ArrJoin:"):
@@ -30,11 +102,15 @@ def getJsonValue(lnjsn, ln):
                 lnjsn = tempVal
             else:
                 lnjsn = ""
+        elif x.startswith("Filename:"):
+            lnjsn = filename
+        elif x.startswith("GetDate:"):
+            lnjsn = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         elif x.startswith("ArrNotHave:"):
             x = x[11:]
             found = False
             for i in range(len(lnjsn)):
-                if getJsonValue(lnjsn[i], x.replace(",",".")) is None:
+                if getJsonValue(lnjsn[i], x.replace(",","."),filename) is None:
                     lnjsn = lnjsn[i]
                     found = True
                     break
@@ -45,7 +121,7 @@ def getJsonValue(lnjsn, ln):
             found = False
             for i in range(len(lnjsn)):
                 if getJsonValue(lnjsn[i], spl[0].replace(",",".")
-                                ) == spl[1].replace(",","."):
+                                ,filename) == spl[1].replace(",","."):
                     lnjsn = lnjsn[i]
                     found = True
                     break
@@ -86,10 +162,10 @@ def getJsonValue(lnjsn, ln):
                 lnjsn = lnjsn[:19].replace("T"," ")
             else:
                 return None
-        elif x in lnjsn:
+        elif x in lnjsn and not isinstance(lnjsn,str):
             lnjsn = lnjsn[x]
         elif is_integer(x):
-            if int(x) < len(lnjsn):
+            if isinstance(lnjsn, list) and int(x) < len(lnjsn):
                 lnjsn = lnjsn[int(x)]
             else:
                 lnjsn = ""
@@ -99,31 +175,93 @@ def getJsonValue(lnjsn, ln):
     return lnjsn
 
 
-def combineValues(jsn, path):
-    retVal = ""
+def combineValues(jsn, path, filename):
+    values = []
     for ln in path.splitlines():
-        retVal = (retVal + " " + str(getJsonValue(jsn, ln) or "")).strip()
-    return retVal
+        value = getJsonValue(jsn, ln, filename)
+        if value:  # This will skip over both None and empty strings
+            values.append(str(value))
+
+    retVal = " ".join(values).strip()
+
+    return retVal if retVal else None
+
+
+
+
+
 
 
 ####### Main #######
-def parse(configpath):
-    logging.info('Started parsing "%s"', configpath)
+def writerow_flex(data,csvwriter,row,outputFormat):
+    if outputFormat =='csv':
+        csvwriter.writerow(row)
+    elif outputFormat == 'parquet' or outputFormat == 'return':
+        data.append(row)
+
+def parse_one_resource(anchor,paths,jsndict,leng,csvwriter,data,filename,outputFormat):
+    thisRow = [None]*leng
+    result_count = 0
+    if anchor == False:
+        for i in range(leng):
+            thisRow[i] = combineValues(jsndict, paths[i],filename)
+        writerow_flex(data,csvwriter,thisRow,outputFormat)
+        return 1
+    else:
+        anchorArray = getJsonValue(jsndict, anchor, filename)
+        if anchorArray is not None and isinstance(anchorArray, list):
+            for z in anchorArray:
+                thisRow = [None] * leng
+                for i in range(leng):
+                    if paths[i][:7] == 'Anchor:':
+                        thisRow[i] = combineValues(z, paths[i][7:],filename)
+                    else:
+                        thisRow[i] = combineValues(jsndict, paths[i],filename)
+                result_count += 1
+                writerow_flex(data, csvwriter, thisRow, outputFormat)
+            return result_count
+        elif anchorArray is not None:
+            for i in range(leng):
+                if paths[i][:7] == 'Anchor:':
+                    thisRow[i] = combineValues(anchorArray,
+                                               paths[i][7:]
+                                               , filename)
+                else:
+                    thisRow[i] = combineValues(jsndict, paths[i], filename)
+
+            writerow_flex(data, csvwriter, thisRow, outputFormat)
+            return 1
+
+
+def parse(configPath,inputPath=None,outputPath=None,missingPath=None,outputFormat=None,inputFormat=None,writeMode=None):
+    f = open(configPath, "r")
+    logging.debug('Started parsing "%s"', configPath)
+
     config = configparser.ConfigParser()
+    try:
+        config.read(configPath)
+    except:
+        logging.exception(f"Failed to read or parse the configuration file: {configPath}. Error: {e}")
+        return
 
-    config.read(configpath)
-    inputPath = config['GenConfig']['inputPath']
-    outputPath = config['GenConfig']['outputPath']
-    if config.has_option('GenConfig', 'anchor'):
-        anchor = config['GenConfig']['anchor']
-    else:
-        anchor = False
-    if config.has_option('GenConfig', 'WriteMode'):
-        if config['GenConfig']['WriteMode'] == 'append':
-            writemode = "a"
-    else:
-        writemode = "w"
 
+
+    anchor = config['GenConfig'].get('anchor', False)
+    inputPath = inputPath or config['GenConfig']['inputPath']
+    outputPath = outputPath or config['GenConfig']['outputPath']
+    missingPath = missingPath or config['GenConfig'].get('missingPath',None)
+    outputFormat = outputFormat or config['GenConfig'].get('outputFormat', 'return')
+    inputFormat = inputFormat or config['GenConfig'].get('inputFormat', 'json')
+
+    if (writeMode or config['GenConfig'].get('writeMode', 'a')).lower() in ['w','write']:
+        writeMode = "w"
+    else:
+        writeMode = "a"
+
+    if inputFormat == 'ndjson' and outputFormat != 'csv':
+        raise ValueError("Input format 'ndjson' is only supported with 'csv' output format.")
+    
+    data = []
     header = []
     paths = []
     leng = 0
@@ -134,56 +272,71 @@ def parse(configpath):
         paths.append(config['Struct'][key])
         leng = leng + 1
 
-    thisRow = [""]*leng
-    csvfile = open(outputPath, writemode, newline='')
-    csvwriter = csv.writer(csvfile,
-                           delimiter=',',
-                           escapechar='\\',
-                           quoting=csv.QUOTE_ALL
-                           )
-    if writemode == "w":
-        csvwriter.writerow(header)
+    if outputFormat == 'csv':
+        csvfile = open(outputPath, writeMode, newline='')
+        csvwriter = csv.writer(csvfile,
+                               delimiter=',',
+                               escapechar='\\',
+                               quoting=csv.QUOTE_ALL
+                               )
+        if writeMode == "w":
+            csvwriter.writerow(header)
+    else:
+        csvfile = None
+        csvwriter = None
 
-    badfile = open('badfile.csv', 'a')
 
-    with open(inputPath) as inputFile:
-        for jsntxt in inputFile:
+
+    if inputFormat == 'ndjson':
+        with open(inputPath) as inputFile:
+            for jsntxt in inputFile:
+                result_count = 0
+                try:
+                    jsndict = json.loads(jsntxt)
+                    result_count = parse_one_resource(anchor, paths, jsndict, leng, csvwriter,data,inputPath,outputFormat)
+                    if missingPath:
+                        compare_and_write_new_paths(jsndict,inputFile,anchor,config,missingPath)
+                except:
+
+                    logging.exception('Issue with input file "%s", see badfile.csv',
+                                      configPath
+                                      )
+                row_count = row_count + (result_count or 0)
+    elif inputFormat == 'json':
+        with open(inputPath) as inputFile:
+            result_count = 0
             try:
-                jsndict = json.loads(jsntxt)
+                jsndict = json.loads(inputFile.read())
+                result_count = parse_one_resource(anchor, paths, jsndict, leng, csvwriter,data,inputPath,outputFormat)
+                if missingPath:
+                    compare_and_write_new_paths(jsndict,inputFile,anchor,config,missingPath)
             except:
-                badfile.write(jsntxt)
-                logging.exception('Issue with input file "%s", see badfile.csv',
-                                  configpath
+                logging.exception('Issue with input file "%s" ',
+                                  configPath
                                   )
-            if anchor == False:
-                for i in range(leng):
-                    thisRow[i] = combineValues(jsndict, paths[i])
-                csvwriter.writerow(thisRow)
-                row_count = row_count + 1
-            else:
-                anchorArray = getJsonValue(jsndict, anchor)
-                if anchorArray is not None and isinstance(anchorArray, list):
-                    for z in anchorArray:
-                        for i in range(leng):
-                            if paths[i][:7] == 'Anchor:':
-                                thisRow[i] = combineValues(z, paths[i][7:])
-                            else:
-                                thisRow[i] = combineValues(jsndict, paths[i])
-                        csvwriter.writerow(thisRow)
-                        row_count = row_count + 1
-                elif anchorArray is not None:
-                    for i in range(leng):
-                        if paths[i][:7] == 'Anchor:':
-                            thisRow[i] = combineValues(anchorArray,
-                                                       paths[i][7:]
-                                                       )
-                        else:
-                            thisRow[i] = combineValues(jsndict, paths[i])
-                    csvwriter.writerow(thisRow)
-                    row_count = row_count + 1
 
-    csvfile.close()
-    logging.info('Finished parsing "%s", %s rows written',
-                 configpath,
+
+            row_count = row_count + (result_count or 0)
+    if outputFormat == 'csv':
+        csvfile.close()
+
+    elif outputFormat == 'parquet':
+        try:
+            import pyarrow as pa
+            import pyarrow.parquet as pq
+            import pandas as pd
+        except ImportError:
+            raise ImportError("Please install pyarrow and pandas to use the 'parquet' output format.")
+        table = pa.Table.from_pandas(pd.DataFrame(data, columns=header))
+        pq.write_table(table, outputPath)
+    elif outputFormat == 'return':
+        try:
+            import pandas as pd
+        except ImportError:
+            raise ImportError("Please install pandas to use the 'return' output format.")
+
+        return pd.DataFrame(data, columns=header)
+    logging.debug('Finished parsing "%s", %s rows written',
+                 configPath,
                  str(row_count)
                  )
