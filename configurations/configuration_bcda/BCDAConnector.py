d72cb5a9ea8ed9f9523ed43176d99e61135a723f
thutuva <101137626+thutuva@users.noreply.github.com>
2024-08-20 11:42:23 -0400

    removed section to import parse fhir, users need to move it manually

--- configurations/configuration_bcda/BCDAConnector.py
+++ configurations/configuration_bcda/BCDAConnector.py
@@ -1,8 +1,3 @@
-# This is just so you can import parseFhir from the parent folder
-# if parseFhir.py is in the same folder as connector this isn't needed.
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 # Import the parser script
 import parseFhir
 
